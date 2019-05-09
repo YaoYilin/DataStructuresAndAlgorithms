8bbd47812b0649969047bcc25ad41d053168483d
Elynn <elynn.yao@qq.com>
2019-05-09 14:30:58 +0800

    Graph and Dijkstra

---
+++ Structure/Graph.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+
+public class Graph
+{
+    public List<Vertex> Vertexs = new List<Vertex>();
+
+    public void AddVertex(Vertex v)
+    {
+        Vertexs.Add(v);
+    }
+
+    public int Dijkstra(Vertex v1, Vertex v2)
+    {
+        v1.MinDistance = 0;
+        PriorityQueue<Vertex> queue = new PriorityQueue<Vertex>();
+        queue.Enqueue(v1);
+        Dictionary<int, Vertex> map = new Dictionary<int, Vertex>();
+        while (queue.Count > 0)
+        {
+            var v = queue.Dequeue();
+            foreach (var edge in v.Edges)
+            {
+                int distance = edge.Distance + v.MinDistance;
+                if(edge.To.MinDistance > distance)
+                {
+                    edge.To.MinDistance = distance;
+                    int i;
+                    if (queue.Contains(edge.To, out i))
+                    {
+                        queue.ShiftUp(i);
+                    }
+                    else
+                    {
+                        queue.Enqueue(edge.To);
+                    }
+                }
+            }
+            map.Add(v.Index, v);
+        }
+        
+        return map[v2.Index].MinDistance;
+    }
+
+    public enum State
+    {
+        NotVisited,
+        Visiting,
+        Visited
+    }
+
+    public class Edge
+    {
+        public Edge(Vertex from, Vertex to, int distance = 1)
+        {
+            From = from;
+            To = to;
+            Distance = distance;
+        }
+
+        public Vertex From
+        {
+            get;
+            private set;
+        }
+
+        public Vertex To
+        {
+            get;
+            private set;
+        }
+
+        public int Distance
+        {
+            get;
+            private set;
+        }
+
+        public override string ToString()
+        {
+            return "From " + From.Index + " To " + To.Index + " Distance = " + Distance;
+        }
+
+    }
+
+    public class Vertex : IPriority
+    {
+        public Vertex(int index)
+        {
+            Index = index;
+            MinDistance = int.MaxValue;
+            State = State.NotVisited;
+        }
+
+        public List<Edge> Edges = new List<Edge>();
+
+        public void AddEdge(Vertex to, int distance = 1)
+        {
+            Edges.Add(new Edge(this, to, distance));
+        }
+
+        public int MinDistance
+        {
+            get;
+            set;
+        }
+
+        public int Index
+        {
+            get;
+            private set;
+        }
+
+        public State State
+        {
+            get;
+            set;
+        }
+
+        public int Priority { get { return MinDistance; } }
+
+        public override string ToString()
+        {
+            return Index.ToString() + " " + MinDistance;
+        }
+    }
+}
+
---
+++ Structure/PriorityQueue.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+public interface IPriority
+{
+    int Priority { get; }
+}
+
+public class PriorityQueue<T> where T : IPriority
+{
+    private List<T> m_Heap;
+
+    public PriorityQueue()
+    {
+        m_Heap = new List<T>();
+    }
+
+    public int Count
+    {
+        get
+        {
+            return m_Heap.Count;
+        }
+    }
+
+    public T Peek()
+    {
+        if (m_Heap.Count <= 0)
+        {
+            return default(T);
+        }
+        return m_Heap[0];
+    }
+
+    public bool Contains(T value, out int index)
+    {
+        for (int i = 0; i < m_Heap.Count; i++)
+        {
+            if(m_Heap[i].Equals(value))
+            {
+                index = i;
+                return true;
+            }
+        }
+        index = -1;
+        return false;
+    }
+
+    public T Dequeue()
+    {
+        if (m_Heap.Count <= 0)
+        {
+            return default(T);
+        }
+        T value = m_Heap[0];
+        m_Heap.RemoveAt(0);
+        Heapify(0);
+        return value;
+    }
+
+    public void Enqueue(T value)
+    {
+        m_Heap.Add(value);
+        ShiftUp(m_Heap.Count - 1);
+    }
+
+    public void ShiftUp(int i)
+    {
+        while (i > 0)
+        {
+            int parent = (i - 1) / 2;
+            if(m_Heap[parent].Priority > m_Heap[i].Priority)
+            {
+                Swap(i, parent);
+                i = parent;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    private void Heapify(int i)
+    {
+        int left = 2 * i + 1;
+        int right = left + 1;
+        int lowest = i;
+        if (left < m_Heap.Count && m_Heap[left].Priority < m_Heap[i].Priority)
+        {
+            lowest = left;
+        }
+        if (right < m_Heap.Count && m_Heap[right].Priority < m_Heap[lowest].Priority)
+        {
+            lowest = right;
+        }
+        if (lowest != i)
+        {
+            Swap(lowest, i);
+            Heapify(lowest);
+        }
+    }
+
+    private void Swap(int i, int j)
+    {
+        T t = m_Heap[i];
+        m_Heap[i] = m_Heap[j];
+        m_Heap[j] = t;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (var i in m_Heap)
+        {
+            sb.Append(i);
+            sb.Append(" ");
+        }
+        return sb.ToString();
+    }
+}
